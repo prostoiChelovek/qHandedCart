7c0eaf3ef292e4f6729c82e8d1fd44dc8be9d711
Acher2001 <sasha.festus2@gmail.com>
2021-10-07 19:15:53 +0500

    2

--- software/gyroTest/main.py
+++ software/gyroTest/main.py
@@ -12,6 +12,12 @@ accelerometer = adafruit_adxl34x.ADXL343(i2c)
 # For ADXL345
 # accelerometer = adafruit_adxl34x.ADXL345(i2c)
 
+accelerometer.enable_motion_detection()
+# alternatively you can specify the threshold when you enable motion detection for more control:
+# accelerometer.enable_motion_detection(threshold=10)
+
 while True:
     print("%f %f %f" % accelerometer.acceleration)
-    time.sleep(0.2)
+
+    print("Motion detected: %s" % accelerometer.events["motion"])
+    time.sleep(0.5)
