1aa282a0f3a64c9d5b641dbb5ea9e6e249a832d2
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2021-10-26 10:30:08 +0500

    create a module init file for Path

---
+++ software/main/Path/__init__.py
@@ -0,0 +1,9 @@
+from .point import Point, PointTpl
+from .line import Line, LineTpl
+from .path import Path
+
+__all__ = [
+        "Point", "PointTpl",
+        "Line", "LineTpl",
+        "Path",
+        ]
