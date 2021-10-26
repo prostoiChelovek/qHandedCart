27a224756d78531593a19af396672dbeeeed5abd
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2021-10-26 10:33:31 +0500

    make Point iteratable and implement custom equality check

--- software/main/Path/point.py
+++ software/main/Path/point.py
@@ -9,10 +9,17 @@ class Point:
     def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
-    
+
     @staticmethod
     def parse(tpl: PointTpl) -> Point:
         return Point(*tpl)
+
+    def __iter__(self):
+        for x in (self.x, self.y):
+            yield x
+
+    def __eq__(self, o: Point) -> bool:
+        return tuple(self) == tuple(o)
 
     def __str__(self) -> str:
         return f"{{{self.x}, {self.y}}}"
