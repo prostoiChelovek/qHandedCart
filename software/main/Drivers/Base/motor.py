63fcc7bb68d919fc5d74ac4fc819db1d14cf703f
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2021-10-30 11:35:03 +0500

    add a getter for Motor's current speed

--- software/main/Drivers/Base/motor.py
+++ software/main/Drivers/Base/motor.py
@@ -7,9 +7,19 @@ class Motor(ABC):
         COUNTERCLOCKWISE = -1
         CLOCKWISE = 1
 
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._speed = 0
+
     def set_speed(self, speed: int) -> None:
         speed_capped = max(0, min(speed, 100))
+        self._speed = speed_capped
         self._set_speed(speed_capped)
+
+    @property
+    def speed(self) -> int:
+        return self._speed
 
     @abstractmethod
     def stop(self) -> None:
