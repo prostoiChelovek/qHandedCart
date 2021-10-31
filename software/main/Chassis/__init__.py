06c902bffe1882d13ffdce7798f3764ea55055f6
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2021-10-31 20:16:45 +0500

    add Chassis to the __init__ file

--- software/main/Chassis/__init__.py
+++ software/main/Chassis/__init__.py
@@ -1,7 +1,8 @@
 from .wheel import Wheel
 from .synchronized_wheels import SynchronizedWheels
 from .chassis_wheels import ChassisWheels
+from .chassis import Chassis
 
 __all__ = [
-        "Wheel", "SynchronizedWheels", "ChassisWheels"
+        "Wheel", "SynchronizedWheels", "ChassisWheels", "Chassis"
         ]
