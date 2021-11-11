143c30fb553c6ae4bacee87cb07e7a11390bfa67
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2021-11-11 22:39:04 +0500

    add a property to get time_delta in seconds

--- software/main/Drivers/Base/encoder.py
+++ software/main/Drivers/Base/encoder.py
@@ -2,11 +2,17 @@ from datetime import timedelta
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
 
+from unum import units
+
 
 @dataclass
 class EncoderData:
     ticks: int
     time_delta: timedelta
+
+    @property
+    def seconds(self) -> units.s:
+        return self.time_delta.total_seconds() * units.s
 
 
 class Encoder(ABC):
