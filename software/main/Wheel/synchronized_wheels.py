57629951c2c2b0c255bb43e7a190c149c873f8e0
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2021-11-12 12:10:06 +0500

    update the SynchronizedWheels class to math the signature of Wheel

--- software/main/Wheel/synchronized_wheels.py
+++ software/main/Wheel/synchronized_wheels.py
@@ -1,20 +1,11 @@
 from typing import Iterable
 
-from ..Drivers.Base import Motor
 from . import Wheel
 
 
 class SynchronizedWheels:
     def __init__(self, motors: Iterable[Wheel]) -> None:
         self._wheels = motors
-
-    def stop(self) -> None:
-        for w in self._wheels:
-            w.stop()
-
-    def set_direction(self, direction: Motor.Direction) -> None:
-        for w in self._wheels:
-            w.set_direction(direction)
 
     def set_speed(self, speed: int) -> None:
         for w in self._wheels:
