bd8c9c2ab76071076eb4ae4863db557c99b07d84
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2021-11-13 10:00:35 +0500

    separate regulators for linear and angular velocity of Chassis

--- software/main/Chassis/chassis.py
+++ software/main/Chassis/chassis.py
@@ -6,12 +6,27 @@ from ..Odometry import Odometry
 from ..Regulators import Regulator
 from ..config import Configurable
 
+class ChassisRegulators:
+    def __init__(self, linear: Regulator, angular: Regulator) -> None:
+        self._linear = linear
+        self._angular = angular
+
+    def set_target(self, velocity: Velocity) -> None:
+        self._linear.set_target(velocity.linear)
+        self._angular.set_target(velocity.angular)
+
+    def compute(self, actual: Velocity) -> Velocity:
+        return Velocity(
+                self._linear.compute(actual.linear),
+                self._angular.compute(actual.angular)
+                )
+
 
 class Chassis(Configurable):
     def __init__(self,
                  wheels: ChassisWheels,
                  odometry: Odometry,
-                 regulator: Regulator,
+                 regulator: ChassisRegulators,
                  kinematics: Kinematics) -> None:
         self._wheels = wheels
         self._odometry = odometry
@@ -28,7 +43,7 @@ class Chassis(Configurable):
         self._odometry.update()
 
         actual_velocity = self._odometry.velocity
-        regulation: Velocity = self._regulator.compute(actual_velocity)
+        regulation = self._regulator.compute(actual_velocity)
         new_velocity = self.__velocity + regulation
         self._apply_velocity(new_velocity)
 
